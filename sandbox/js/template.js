04037ce84494553e19ec2b8ed30ae79b556b40ff
tishitani <ishitani@sh-concierge.com>
2020-06-01 21:36:55 +0900

    マスタッシュ、ディレクティブ復習

---
+++ sandbox/js/template.js
@@ -0,0 +1,7 @@
+// マスタッシュ構文でHello Worldを表示
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World!'
+    }
+});
---
+++ sandbox/js/text.js
@@ -0,0 +1,7 @@
+// ディレクティブでHello Worldを表示
+let app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World!'
+    }
+});
